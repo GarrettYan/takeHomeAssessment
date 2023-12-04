3fb2681c18986d1681600f5adf16b44973379dc5
GarrettYan <114932225+GarrettYan@users.noreply.github.com>
2023-12-04 00:48:47 -0500

    Add sorting functionality to Metrics component

--- stackline-garrett/src/components/Metrics.tsx
+++ stackline-garrett/src/components/Metrics.tsx
@@ -1,18 +1,66 @@
 import React, { useState, useEffect } from 'react';
 
 const Metrics = ({ sales }) => {
+  const [sortedSales, setSortedSales] = useState([]);
+  const [sortConfig, setSortConfig] = useState({ key: null, direction: 'asc' });
+
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     maximumFractionDigits: 0,
   });
 
+  const requestSort = (key) => {
+    let direction = 'asc';
+    if (sortConfig.key === key && sortConfig.direction === 'asc') {
+      direction = 'desc';
+    }
+    setSortConfig({ key, direction });
+
+    const sortedData = [...sales].sort((a, b) => {
+      if (a[key] < b[key]) {
+        return direction === 'asc' ? -1 : 1;
+      }
+      if (a[key] > b[key]) {
+        return direction === 'asc' ? 1 : -1;
+      }
+      return 0;
+    });
+
+    setSortedSales(sortedData);
+  };
+
+  const getHeader = (key, label) => {
+    return (
+      <th onClick={() => requestSort(key)}>
+        {label}
+        {sortConfig.key === key
+          ? sortConfig.direction === 'asc'
+            ? ' ▲'
+            : ' ▼'
+          : ' ⇵'}
+      </th>
+    );
+  };
+
+  useEffect(() => {
+    setSortedSales(sales);
+  }, [sales]);
+
   return (
     <div className='metrics'>
       <table>
-        <thead></thead>
+        <thead>
+          <tr>
+            {getHeader('weekEnding', 'Week Ending')}
+            {getHeader('retailSales', 'Retail Sales')}
+            {getHeader('wholesaleSales', 'Wholesale Sales')}
+            {getHeader('unitsSold', 'Units Sold')}
+            {getHeader('retailerMargin', 'Retailer Margin')}
+          </tr>
+        </thead>
         <tbody>
-          {sales.map((sale, index) => (
+          {sortedSales.map((sale, index) => (
             <tr key={index}>
               <td>{sale.weekEnding}</td>
               <td>{formatter.format(sale.retailSales)}</td>
