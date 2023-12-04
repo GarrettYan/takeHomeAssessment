5b4489fbd425ac283308d683a129c3ddda44f831
GarrettYan <114932225+GarrettYan@users.noreply.github.com>
2023-12-04 00:06:29 -0500

    implement data to Metrics.tsx and ProductInfo.tsx

--- stackline-garrett/src/components/Metrics.tsx
+++ stackline-garrett/src/components/Metrics.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Metrics: React.FC = () => {
+const Metrics = ({ sales }) => {
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     maximumFractionDigits: 0,
   });
 
-  return <div className='metrics'></div>;
+  return (
+    <div className='metrics'>
+      <table>
+        <thead></thead>
+        <tbody>
+          {sales.map((sale, index) => (
+            <tr key={index}>
+              <td>{sale.weekEnding}</td>
+              <td>{formatter.format(sale.retailSales)}</td>
+              <td>{formatter.format(sale.wholesaleSales)}</td>
+              <td>{sale.unitsSold}</td>
+              <td>{formatter.format(sale.retailerMargin)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Metrics;
--- stackline-garrett/src/components/ProductInfo.tsx
+++ stackline-garrett/src/components/ProductInfo.tsx
@@ -1,7 +1,19 @@
+// Header.tsx
 import React from 'react';
 
-const ProductInfo: React.FC = () => {
-  return <div className='product-info'></div>;
+const ProductInfo: React.FC = ({ image, title, subtitle, tags }) => {
+  return (
+    <div className='product-info'>
+      <img src={image} alt={title} />
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
+      <ul>
+        {tags.map((tag) => (
+          <li>{tag}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ProductInfo;
